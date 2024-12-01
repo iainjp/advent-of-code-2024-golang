ea7d80f0aadcecc50332ae803272ccf29a2765ac
Iain Pritchard <iainpritchard92@gmail.com>
2024-12-01 19:22:11 +0000

    Day1 test passing, now wiring input

--- day-1/solution.go
+++ day-1/solution.go
@@ -1,11 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println("Hello world!")
 }
 
+func abs(v int) int {
+	return max(v, -v)
+}
+
 func SumDistances(left, right []int) int {
-	return 0
+	sort.Ints(left)
+	sort.Ints(right)
+
+	total := 0
+
+	for index, leftNum := range left {
+		distance := abs(leftNum - right[index])
+		total += distance
+	}
+
+	return total
 }
