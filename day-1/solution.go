ee2a6f769478a250573fe0492a35ee32490a1fea
Iain Pritchard <iainpritchard92@gmail.com>
2024-12-01 19:46:40 +0000

    Completed day-1 part 1

--- day-1/solution.go
+++ day-1/solution.go
@@ -1,12 +1,50 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	input := getInput("input.txt")
+	result := SumDistances(input.left, input.right)
+	fmt.Printf("Got result: %v\n", result)
+}
+
+type Input struct {
+	left, right []int
+}
+
+func getInput(filename string) Input {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var left []int
+	var right []int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		items := strings.Split(line, "   ")
+
+		leftInt, _ := strconv.Atoi(items[0])
+		left = append(left, leftInt)
+
+		rightInt, _ := strconv.Atoi(items[1])
+		right = append(right, rightInt)
+	}
+
+	return Input{left: left, right: right}
+
 }
 
 func abs(v int) int {
