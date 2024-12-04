61b5ee866688d7b9ba3c441336514eb7f6d8549d
Iain Pritchard <iainpritchard92@gmail.com>
2024-12-04 16:55:49 +0000

    Solution for day4 part1

--- day-4/solution.go
+++ day-4/solution.go
@@ -5,10 +5,23 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
 var ErrInputFile = errors.New("cannot open input file")
+
+type Point struct {
+	x, y int
+}
+
+type Input struct {
+	maxX int
+	maxY int
+	grid GridMap
+}
+
+type GridMap = map[Point]string
 
 func GetInput(filename string) (*Input, error) {
 	file, err := os.Open(filename)
@@ -21,36 +34,151 @@ func GetInput(filename string) (*Input, error) {
 
 	var maxX int
 	var y = 0
-	grid := make(map[Coord]string, 1000)
+	grid := make(GridMap, 1000)
 	for scanner.Scan() {
 		line := scanner.Text()
-		maxX = len(line) - 1
+		maxX = len(line)
 
 		for x, c := range strings.Split(line, "") {
-			coord := Coord{x, y}
+			coord := Point{x, y}
 			grid[coord] = c
 		}
 		y++
 	}
 
-	return &Input{maxX: maxX, maxY: y - 1, grid: grid}, nil
+	maxY := y
+	fmt.Printf("got maxX=<%v>, maxY=<%v>", maxX, maxY)
+	return &Input{maxX: maxX, maxY: maxY, grid: grid}, nil
 }
 
 func main() {
 	input, _ := GetInput("input.txt")
-	fmt.Printf("Got input: %v", input)
+	p1Result := Part1(input)
+	fmt.Printf("Part 1: got %v\n", p1Result)
 }
 
-func Part1(input string) int {
-	return 0
+func Part1(input *Input) int {
+	// TODO
+	// 1. get starting points of X
+	// 2. get combinations of 4 letters in row (up,down,left,right,diagonals) from starting point
+	// 3. count those that spell "XMAS"
+	// 4. donezo.
+
+	startingPoints := GetCoordsOfLetter(input.grid, "X")
+	counter := 0
+
+	for _, p := range startingPoints {
+		sequencesToConsider := GetSeqOfPointsSurrounding(p, input.maxX, input.maxY)
+
+		for _, ps := range sequencesToConsider {
+			if GetPointsAsString(input.grid, ps...) == "XMAS" {
+				// fmt.Printf("Found XMAS at: %v\n", ps)
+				counter++
+			}
+		}
+	}
+
+	return counter
 }
 
-type Coord struct {
-	x, y int
+func GetPointsAsString(grid GridMap, ps ...Point) string {
+	var result []string
+
+	for _, p := range ps {
+		result = append(result, grid[p])
+	}
+
+	return strings.Join(result, "")
 }
 
-type Input struct {
-	maxX int
-	maxY int
-	grid map[Coord]string
+func GetSeqOfPointsSurrounding(p Point, maxX, maxY int) [][]Point {
+	var results [][]Point
+	spaceLeft := p.x >= 3
+	spaceRight := p.x+3 <= maxX
+	spaceUp := p.y >= 3
+	spaceDown := p.y+3 <= maxY
+	spaceUL := spaceUp && spaceLeft
+	spaceDL := spaceDown && spaceLeft
+	spaceUR := spaceUp && spaceRight
+	spaceDR := spaceDown && spaceRight
+
+	if spaceLeft {
+		seq := []Point{p, Point{p.x - 1, p.y}, Point{p.x - 2, p.y}, Point{p.x - 3, p.y}}
+		results = append(results, seq)
+	}
+
+	if spaceRight {
+		seq := []Point{p, Point{p.x + 1, p.y}, Point{p.x + 2, p.y}, Point{p.x + 3, p.y}}
+		results = append(results, seq)
+	}
+
+	if spaceUp {
+		seq := []Point{p, Point{p.x, p.y - 1}, Point{p.x, p.y - 2}, Point{p.x, p.y - 3}}
+		results = append(results, seq)
+	}
+
+	if spaceDown {
+		seq := []Point{p, Point{p.x, p.y + 1}, Point{p.x, p.y + 2}, Point{p.x, p.y + 3}}
+		results = append(results, seq)
+	}
+
+	if spaceUL {
+		seq := []Point{p, Point{p.x - 1, p.y - 1}, Point{p.x - 2, p.y - 2}, Point{p.x - 3, p.y - 3}}
+		results = append(results, seq)
+	}
+
+	if spaceDL {
+		seq := []Point{p, Point{p.x - 1, p.y + 1}, Point{p.x - 2, p.y + 2}, Point{p.x - 3, p.y + 3}}
+		results = append(results, seq)
+	}
+
+	if spaceUR {
+		seq := []Point{p, Point{p.x + 1, p.y - 1}, Point{p.x + 2, p.y - 2}, Point{p.x + 3, p.y - 3}}
+		results = append(results, seq)
+	}
+
+	if spaceDR {
+		seq := []Point{p, Point{p.x + 1, p.y + 1}, Point{p.x + 2, p.y + 2}, Point{p.x + 3, p.y + 3}}
+		results = append(results, seq)
+	}
+
+	return results
+}
+
+func GetCoordsOfLetter(grid GridMap, letter string) []Point {
+	var result []Point
+
+	for p, v := range grid {
+		if v == letter {
+			result = append(result, p)
+		}
+	}
+
+	return result
+}
+
+func pointCmp(l, r Point) int {
+	if l.y < r.y {
+		return -1
+	}
+	if l.y == r.y {
+		if l.x < r.x {
+			return -1
+		}
+	}
+	return 1
+}
+
+// get keys ordered by y, x - like scanning along a big line
+func GetOrderedKeys(dict GridMap) []Point {
+	keys := make([]Point, len(dict))
+	i := 0
+	for k := range dict {
+		keys[i] = k
+		i++
+	}
+
+	slices.SortFunc(keys, pointCmp)
+
+	return keys
 }
--- day-4/solution_test.go
+++ day-4/solution_test.go
@@ -9,19 +9,19 @@ import (
 func TestGetInput(t *testing.T) {
 	testInputFilename := "input_test.txt"
 
-	wantedGrid := map[Coord]string{
-		Coord{0, 0}: "M",
-		Coord{1, 0}: "M",
-		Coord{2, 0}: "M",
-		Coord{3, 0}: "S",
-		Coord{0, 1}: "M",
-		Coord{1, 1}: "S",
-		Coord{2, 1}: "A",
-		Coord{3, 1}: "M",
-		Coord{0, 2}: "A",
-		Coord{1, 2}: "M",
-		Coord{2, 2}: "X",
-		Coord{3, 2}: "S",
+	wantedGrid := GridMap{
+		{0, 0}: "M",
+		{1, 0}: "M",
+		{2, 0}: "M",
+		{3, 0}: "S",
+		{0, 1}: "M",
+		{1, 1}: "S",
+		{2, 1}: "A",
+		{3, 1}: "M",
+		{0, 2}: "A",
+		{1, 2}: "M",
+		{2, 2}: "X",
+		{3, 2}: "S",
 	}
 
 	want := &Input{
@@ -30,6 +30,68 @@ func TestGetInput(t *testing.T) {
 		grid: wantedGrid,
 	}
 	got, _ := GetInput(testInputFilename)
+
+	utils.CheckEqual(got, want, t)
+}
+
+func TestGetCoordsOfLetter(t *testing.T) {
+	grid := GridMap{
+		{0, 0}: "M",
+		{1, 0}: "M",
+		{2, 0}: "M",
+		{3, 0}: "S",
+		{0, 1}: "M",
+		{1, 1}: "S",
+		{2, 1}: "A",
+		{3, 1}: "M",
+		{0, 2}: "A",
+		{1, 2}: "M",
+		{2, 2}: "X",
+		{3, 2}: "S",
+	}
+
+	want := []Point{
+		{2, 1},
+		{0, 2},
+	}
+
+	got := GetCoordsOfLetter(grid, "A")
+
+	utils.CheckEqual(got, want, t)
+}
+
+func TestGetOrderedKeys(t *testing.T) {
+	grid := GridMap{
+		{0, 0}: "M",
+		{1, 0}: "M",
+		{2, 0}: "M",
+		{3, 0}: "S",
+		{0, 1}: "M",
+		{1, 1}: "S",
+		{2, 1}: "A",
+		{3, 1}: "M",
+		{0, 2}: "A",
+		{1, 2}: "M",
+		{2, 2}: "X",
+		{3, 2}: "S",
+	}
+
+	want := []Point{
+		{0, 0},
+		{1, 0},
+		{2, 0},
+		{3, 0},
+		{0, 1},
+		{1, 1},
+		{2, 1},
+		{3, 1},
+		{0, 2},
+		{1, 2},
+		{2, 2},
+		{3, 2},
+	}
+
+	got := GetOrderedKeys(grid)
 
 	utils.CheckEqual(got, want, t)
 }
