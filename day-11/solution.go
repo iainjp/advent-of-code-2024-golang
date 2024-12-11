6db0a78cd74d853c7f2e73af828db1bbca6a93b3
Iain Pritchard <iainpritchard92@gmail.com>
2024-12-11 11:23:58 +0000

    Day11Part1: Add stone linkedlist and tests

--- day-11/solution.go
+++ day-11/solution.go
@@ -11,6 +11,40 @@ import (
 
 var ErrInputFile = errors.New("cannot open input file")
 
+type Stone struct {
+	prev   *Stone
+	next   *Stone
+	number int
+}
+
+type StoneLine struct {
+	head *Stone
+}
+
+func (sl *StoneLine) ToSlice() []Stone {
+	current := sl.head
+	stoneSlice := []Stone{*current}
+
+	for current.next != nil {
+		current = current.next
+		stoneSlice = append(stoneSlice, *current)
+	}
+
+	return stoneSlice
+}
+
+func (sl *StoneLine) GetNumbers() []int {
+	current := sl.head
+	resultSlice := []int{current.number}
+
+	for current.next != nil {
+		current = current.next
+		resultSlice = append(resultSlice, current.number)
+	}
+
+	return resultSlice
+}
+
 type Input struct {
 	stones []int
 }
@@ -34,6 +68,26 @@ func GetInput(filename string) (*Input, error) {
 	}
 
 	return &Input{stones: stones}, nil
+}
+
+// build LinkedList of stones, returning head
+func BuildStones(ints []int) *StoneLine {
+	var head *Stone
+	var prev *Stone
+	for i, n := range ints {
+		stone := &Stone{
+			number: n,
+		}
+		if i == 0 {
+			head = stone
+		} else {
+			stone.prev = prev
+			prev.next = stone
+		}
+		prev = stone
+	}
+
+	return &StoneLine{head: head}
 }
 
 func main() {
--- day-11/solution_test.go
+++ day-11/solution_test.go
@@ -12,3 +12,51 @@ func TestGetInput(t *testing.T) {
 
 	utils.CheckEqual(got, want, t)
 }
+
+func TestBuildStones(t *testing.T) {
+	input := []int{1, 2, 3}
+	want := []int{1, 2, 3}
+
+	got := BuildStones(input)
+	gotNumbers := got.GetNumbers()
+
+	utils.CheckSlicesHaveSameElements(gotNumbers, want, t)
+}
+
+func TestStoneLine(t *testing.T) {
+
+	t.Run("GetNumbers() returns numbers", func(t *testing.T) {
+		stone1 := Stone{number: 1}
+		stone2 := Stone{number: 2}
+		stone1.next = &stone2
+		stone2.prev = &stone1
+		stone3 := Stone{number: 3}
+		stone2.next = &stone3
+		stone3.prev = &stone2
+
+		stoneLine := StoneLine{head: &stone1}
+
+		want := []int{1, 2, 3}
+		got := stoneLine.GetNumbers()
+
+		utils.CheckSlicesHaveSameElements(got, want, t)
+	})
+
+	t.Run("ToSlice() returns slice", func(t *testing.T) {
+		stone1 := Stone{number: 1}
+		stone2 := Stone{number: 2}
+		stone1.next = &stone2
+		stone2.prev = &stone1
+		stone3 := Stone{number: 3}
+		stone2.next = &stone3
+		stone3.prev = &stone2
+
+		stoneLine := StoneLine{head: &stone1}
+
+		want := []Stone{stone1, stone2, stone3}
+		got := stoneLine.ToSlice()
+
+		utils.CheckSlicesHaveSameElements(got, want, t)
+	})
+
+}
