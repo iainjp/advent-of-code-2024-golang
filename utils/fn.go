a392fe971b68caf97d7eeaf11b18d5e1cdc0b784
Iain Pritchard <iainpritchard92@gmail.com>
2024-12-17 21:03:42 +0000

    Small refactor to /utils package

--- utils/fn.go
+++ utils/fn.go
@@ -2,9 +2,6 @@ package utils
 
 import (
 	"iter"
-	"reflect"
-	"slices"
-	"testing"
 )
 
 func Abs(v int) int {
@@ -63,37 +60,6 @@ func CountOccurences[T comparable](ts []T) map[T]int {
 		counts[num] = counts[num] + 1
 	}
 	return counts
-}
-
-func CheckEqual[K any](got, want K, t testing.TB) {
-	t.Helper()
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("wanted %v, got %v", want, got)
-	}
-}
-
-func CheckContains[K comparable, E ~[]K](slice E, item K, t testing.TB) {
-	t.Helper()
-	if !slices.Contains(slice, item) {
-		t.Fatalf("%v does not contain %v", slice, item)
-	}
-}
-
-func CheckNotNil[K any](got *K, t testing.TB) {
-	t.Helper()
-	if got == nil {
-		t.Fatal("expected not nil, got nil")
-	}
-}
-
-func CheckSlicesHaveSameElements[S ~[]E, E comparable](got S, want S, t testing.TB) {
-	t.Helper()
-	CheckEqual(len(got), len(want), t)
-	for _, e := range want {
-		if !slices.Contains(got, e) {
-			t.Fatalf("<got> does not contain required element from <want>: %v", e)
-		}
-	}
 }
 
 func IterSeqToSlice[E any](it iter.Seq[E]) []E {
