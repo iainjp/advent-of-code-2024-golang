9cc82a33a438927b9a5003ac25f50825486272f4
Iain Pritchard <iainpritchard92@gmail.com>
2024-12-18 14:50:10 +0000

    Day16Part2: parking Dijkstras for other algorithm, given complexity with direction and cost

--- day-16/solution.go
+++ day-16/solution.go
@@ -4,8 +4,12 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"math"
 	"os"
+	"sort"
 	"strings"
+
+	"iain.fyi/aoc2024/utils"
 )
 
 var ErrInputFile = errors.New("cannot open input file")
@@ -18,20 +22,124 @@ const (
 )
 
 const (
-	NORTH = iota
-	EAST  = iota
-	SOUTH = iota
-	WEST  = iota
+	_     = iota
+	NORTH = iota // 1
+	EAST  = iota // 2
+	SOUTH = iota // 3
+	WEST  = iota // 4
 )
 
 type Tile struct {
 	symbol string
 	// indexed by directional const
 	neighbours []*Tile
+	coord      Coord
+	cost       int
+}
+
+func (a Tile) Equal(o Tile) bool {
+	return a.coord == o.coord && a.cost == o.cost
 }
 
 type Maze struct {
 	start *Tile
+	end   *Tile
+	all   []*Tile
+}
+
+// Returns map of *Tile -> cost to get there
+func (m *Maze) Dijkstra() map[*Tile]int {
+	direction := EAST
+	start := m.start
+
+	distances := make(map[*Tile]int, len(m.all))
+	for _, tile := range m.all {
+		distances[tile] = math.MaxInt32
+	}
+	distances[start] = 0
+
+	// set initial costs and distances
+	for i, n := range start.neighbours {
+		if n == nil {
+			continue
+		}
+		if i == direction {
+			n.cost = 1
+			distances[n] = 1
+		}
+		// single turn
+		if utils.Abs(direction-i) == 1 || utils.Abs(direction-1) == 3 {
+			n.cost = 1001
+			distances[n] = 1001
+		}
+
+		// 2 turns
+		if utils.Abs(direction-i) == 2 {
+			n.cost = 2001
+			distances[n] = 2001
+		}
+	}
+
+	var options []*Tile
+	for _, n := range start.neighbours {
+		if n != nil {
+			options = append(options, n)
+		}
+	}
+
+	type Step struct {
+		coord Coord
+		cost  int
+	}
+
+	tileSeen := make(map[Step]bool, len(m.all))
+	// not sure on this condition
+	for len(options) > 0 {
+		sort.SliceStable(options, func(i, j int) bool {
+			return distances[options[i]] < distances[options[j]]
+		})
+
+		curr := options[0]
+		options = options[1:]
+
+		for i, next := range curr.neighbours {
+			if next == nil {
+				continue
+			}
+
+			if i == direction {
+				next.cost = 1
+			}
+			// single turn
+			if utils.Abs(direction-i) == 1 || utils.Abs(direction-i) == 3 {
+				next.cost = 1001
+				direction = i
+			}
+
+			// 2 turns
+			if utils.Abs(direction-i) == 2 {
+				next.cost = 2001
+				direction = i
+			}
+
+			alt := distances[curr] + next.cost
+			if alt < distances[next] {
+				distances[next] = alt
+			}
+			// add to options if we haven't yet considered it at current cost previously
+			step := Step{
+				coord: next.coord,
+				cost:  next.cost,
+			}
+			seen := tileSeen[step]
+			if !seen {
+				tileSeen[step] = true
+				options = append(options, next)
+			}
+		}
+	}
+
+	return distances
 }
 
 type Input struct {
@@ -82,6 +190,7 @@ func GetInput(filename string) (*Input, error) {
 			tile := Tile{
 				symbol:     c,
 				neighbours: make([]*Tile, 4),
+				coord:      coord,
 			}
 			register[coord] = &tile
 			x++
@@ -93,14 +202,15 @@ func GetInput(filename string) (*Input, error) {
 	}
 
 	var start *Tile
-
+	var end *Tile
+	tiles := make(map[*Tile]bool)
 	// create graph
 	for k, v := range register {
 		adj := k.Adjacent()
 		for i, n := range adj {
 			t, ok := register[n]
+			// if not in register (e.g. out-of-bounds) OR wall
 			if !ok || t.symbol == WALL {
-				// set to nil if not in register (e.g. out-of-bounds) OR wall
 				v.neighbours[i] = nil
 				continue
 			}
@@ -109,17 +219,32 @@ func GetInput(filename string) (*Input, error) {
 				start = t
 			}
 
+			if t.symbol == END {
+				end = t
+			}
+
+			tiles[t] = true
 			v.neighbours[i] = t
+		}
+	}
+
+	var all []*Tile
+	for t, v := range tiles {
+		if v {
+			all = append(all, t)
 		}
 	}
 
 	return &Input{
 		maze: &Maze{
 			start: start,
+			end:   end,
+			all:   all,
 		},
 	}, nil
 }
 
 func Part1(input *Input) int {
-	return len(input.maze.start.neighbours)
+	distances := input.maze.Dijkstra()
+	return distances[input.maze.start]
 }
--- day-16/solution_test.go
+++ day-16/solution_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"testing"
 
 	"iain.fyi/aoc2024/utils"
@@ -10,4 +11,17 @@ func TestGetInput(t *testing.T) {
 	input, _ := GetInput("input_example.txt")
 
 	utils.CheckNotNil(input, t)
+	utils.CheckNotNil(input.maze.start, t)
+	utils.CheckNotNil(input.maze.end, t)
+	utils.CheckEqual(len(input.maze.all), 104, t)
+}
+
+func TestPart1(t *testing.T) {
+	input, _ := GetInput("input_example.txt")
+
+	want := 7036
+	got := Part1(input)
+
+	utils.CheckEqual(got-math.MaxInt32, want, t)
+
 }
